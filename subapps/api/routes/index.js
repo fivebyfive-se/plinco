75412687342c7e847af8ad9cc4182bfbaeb39cc1
Kalle Räisänen <kalle@fivebyfive.se>
2020-11-22 16:39:09 +0100

    Add lti-scaffolding

--- subapps/api/routes/index.js
+++ subapps/api/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 
+const lti_app = require('./lti');
+
 const router = Router();
 
 router
     .get('/', (req, res) => res.sendStatus(200))
+    .use('/lti', lti_app)
 ;
 
 module.exports = router;
- 
