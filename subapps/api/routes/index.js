c8ee0db72f2e558b89a29e4963116d4c3321648a
Kalle Räisänen <kalle@fivebyfive.se>
2020-11-29 18:08:00 +0100

    CRUD-scaff

--- subapps/api/routes/index.js
+++ subapps/api/routes/index.js
@@ -1,12 +1,20 @@
 const { Router } = require('express');
 
-const lti_app = require('./lti');
+const injectmodels = require('$middleware/inject-models');
 
+const route_lti = require('./lti');
+const route_mail = require('./mail');
+const route_post = require('./post');
 const router = Router();
 
 router
+    .use(injectmodels)
+    
+    .use('/lti', route_lti)
+    .use('/mail', route_mail)
+    .use('/post', route_post)
+
     .get('/', (req, res) => res.sendStatus(200))
-    .use('/lti', lti_app)
 ;
 
 module.exports = router;
---
+++ subapps/api/routes/post/index.js
@@ -0,0 +1,46 @@
+const { Router } = require('express');
+const router = Router();
+
+const Permissions = require('$models/enums/permissions');
+
+module.exports = router
+    .get('/:postId/:requestToken?', async (req, res) => {
+        const { post, user } = req.requestModels;
+        const visible = post ? post.visibleTo(user) : false;
+
+        if (!visible) {
+            res.sendStatus(400).send();
+        } else {
+            res.send(post);
+        }
+    })
+
+    .post('/:requestToken', async (req, res) => {
+        const { user } = req.requestModels;
+        const { post } = req.body;
+        
+        const circle = await post.getCircle();
+        const canEdit = circle && await circle.editableBy(user);
+
+        if (canEdit) {
+            const [ record ] = await req.models.Post.upsert(post);
+            res.send(record);
+        } else {
+            res.sendStatus(400).send();
+        }
+    })
+
+    .delete('/:postId/:requestToken', async (req, res) => {
+        const { user, post } = req.requestModels;
+        
+        if (await post.editableBy(user)) {
+            res.send(await req.models.Post.destroy(post));
+        } else {
+            res.sendStatus(400).send();
+        }
+    })
+    
+    .get('/suggest/:requestToken', async (req, res) => {
+        res.send([ ]); // list of suggested posts
+    })
+;
